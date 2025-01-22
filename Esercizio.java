ef14421d42e06c087a41701ca629ed4da0e69b81
SimMan123 <simone.manzari2@marconibari.edu.it>
2025-01-22 14:03:23 +0000

    11.3

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,42 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
 
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
-
-// Classe principale, con metodo main
-class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
-
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
-
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+public static char controllo(char o) {
+    Scanner input = new Scanner (System.in);
+    while (o!='a' && o!='s' && o!='r' && o!='e' && o!='v' && o!='x') {
+        System.out.println("Inserire un elemento in una posizione specificata >> a" + "Eliminare un elemento da una posizione specificata >> s" + "Ricerca di un valore >> r" + "Eliminare tutti i duplicati dal vettore >> e" + "Visualizzare il vettore >> v" + "Uscire dal programma >> x ");
+        o = input.next().charAt(0);
     }
+    return o;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+class esercizio {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n, i, j, c, d;
+        char o;
+
+        n = input.nextInt();
+        int[] v = new int[n];
+
+        for (i = 0; i <= n - 1; i++) {
+            v[i] = 0;
+        }
+        do {
+            System.out.println("Inserire un elemento in una posizione specificata >> a" + "Eliminare un elemento da una posizione specificata >> s" + "Ricerca di un valore >> r" + "Eliminare tutti i duplicati dal vettore >> e" + "Visualizzare il vettore >> v" + "Uscire dal programma >> x ");
+            o = input.next().charAt(0);         
+            o=Character.toLowerCase(o);
+            o = controllo(o);
+            if (o=='a') {
+            } else {
+                if (o=='s') {
+                } else {
+                    if (o=='r') {
+                    }
+                }
+            }
+        } while (o!='x');
+    }
+    
+   
+}
